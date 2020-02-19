78c015aa4fe92dfd7e10c66fb2c350cbc0e02ef3
Frank <diaowangzhen@163.com>
2020-02-19 12:00:58 +0800

    A: 读取文件的链式调用示例

---
+++ example/file_chainable.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 链式调用示例
+ * Example of chained calls
+ */
+
+
+use htmlDiff\exceptions\InputException;
+use htmlDiff\Exceptions\MissingParameterException;
+use htmlDiff\HtmlDiff;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$old = __DIR__ . "/html/old_file.html";
+$new = __DIR__."/html/new_file.html";
+
+try {
+    $diff = new HtmlDiff();
+    $res = $diff->loadOldFile($old)->loadNewFile($new)->diff();
+} catch (MissingParameterException $e) {
+    print_r($e->getMessage());die;
+} catch (InputException $e) {
+    print_r($e->getMessage());die;
+}
+
+print_r($res);
+
