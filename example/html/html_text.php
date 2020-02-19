1e727eab75d97c164735653488d6b8e675cdf1e5
Frank <diaowangzhen@163.com>
2020-02-19 15:39:18 +0800

    U: 示例文件

--- example/html/html_text.php
+++ example/html/html_text.php
@@ -1,19 +1,5 @@
 <?php
 
-$old = <<<HTML
-<p>嘿嘿嘿</p>
-<p>肺部有结核哦</p>
-<p>肺肺肺</p>
-<p><img src="https://img.php.cn/upload/article/000/000/020/5d43ad2a508b8873.jpg" alt="" width="270" height="108"></p>
-<p>&nbsp;</p>
-<p>大撒大撒</p>
-HTML;
+$old = file_get_contents(__DIR__."/old_file.html");
 
-$new = <<<HTML
-<p>嘿哈嘿</p>
-<p>肺部没结核吧</p>
-<p>肺堵塞</p>
-<p><img src="https://www.hello-world.ren/usr/themes/variant/images/logo.png" alt="测试图片2" width="113" height="88"></p>
-<p><audio style="display: none;" controls="controls"></audio></p>
-HTML;
-
+$new = file_get_contents(__DIR__."/new_file.html");
